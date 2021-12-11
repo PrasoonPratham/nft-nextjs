92161787d2e72b71b3cd19dd767de49442682959
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2021-12-11 22:59:47 +0530

    init

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,12 @@
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx}',
-    './components/**/*.{js,ts,jsx,tsx}',
-  ],
+  mode: "jit",
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
   },
+  variants: {
+    extend: {},
+  },
   plugins: [],
-}
+};
