18a67f49634f34f61483150775cb76eb3f3d5a3b
Pratham Prasoon <42303128+PrasoonPratham@users.noreply.github.com>
2021-12-12 07:53:35 +0530

    head added

--- pages/_app.js
+++ pages/_app.js
@@ -1,22 +1,23 @@
-import { ThirdwebProvider } from "@3rdweb/react";
+import { ThirdwebProvider } from '@3rdweb/react';
 import 'regenerator-runtime/runtime';
-
-import "tailwindcss/tailwind.css";
+import 'tailwindcss/tailwind.css';
 
 const supportedChainIds = [1, 4, 137];
 const connectors = {
-  injected: {},
+    injected: {}
 };
 
 const MyApp = ({ Component, pageProps }) => {
-  return (
-    <ThirdwebProvider
-      connectors={connectors}
-      supportedChainIds={supportedChainIds}
-    >
-      <Component {...pageProps} />
-    </ThirdwebProvider>
-  );
+    return (
+        <>
+            <ThirdwebProvider
+                connectors={connectors}
+                supportedChainIds={supportedChainIds}
+            >
+                <Component {...pageProps} />
+            </ThirdwebProvider>
+        </>
+    );
 };
 
 export default MyApp;
